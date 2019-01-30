9a8607f582d254f786fe17b97db4854f06cd2b8e
Luke Newsome <luke@newsome.org>
2019-01-29 19:54:37 -0600

    Got all the UI set up

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Luke Newsome on 1/29/19.
+//  Copyright © 2019 Zappy Code. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var recordButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
